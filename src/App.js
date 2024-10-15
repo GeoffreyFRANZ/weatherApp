5b6ccb2005f326a46020a076bb885c6ff9a1025c
GeoffreyFRANZ <61542992+GeoffreyFRANZ@users.noreply.github.com>
2024-10-15 15:07:01 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import Geolocalisation from "./component/Geolocalisation";
 
 function App() {
+  const  [data, setData] =  useState()
+
+  useEffect(()=> {
+     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Paris,fr&appid=b336af79478736d4ca65a9ce41592a41`)
+          .then(response => response.json())
+          .then(data => setData(data));
+
+  },  [])
+  console.log(data)
   return (
     <div className="App">
       <header className="App-header">
@@ -9,7 +20,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
+
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
@@ -17,6 +30,7 @@ function App() {
         >
           Learn React
         </a>
+        <Geolocalisation/>
       </header>
     </div>
   );
